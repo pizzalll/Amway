79604451aa303e5ccd540b01733b0a01a088cd64
pizzalll <58722972+pizzalll@users.noreply.github.com>
2019-12-10 18:09:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    // 마우스 오버시 메뉴 열림
+    $('.depth1 > li').mouseover(function(){
+      // class추가되면 보임
+      $('.depth2').addClass('show');
+    });
+  
+    // 마우스가 나가면 메뉴 닫힘
+    $('.depth1 > li').mouseout(function(){
+      $('.depth2').removeClass('show');
+    });
+  
+  });
